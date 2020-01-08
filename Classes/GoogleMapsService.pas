5727c2483b2b050c880d800da63aaa24d7fc83d2
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-01-08 11:21:18 -0300

    Add distance method

--- Classes/GoogleMapsService.pas
+++ Classes/GoogleMapsService.pas
@@ -13,11 +13,13 @@ type TGoogleMapsService = class
 private
   class function StripNonXml(s: string): string;
   class function ParseCoordinatesXML(xml :String) :TCoordinates;
+  class function ParseDistancesXML(xml :String) :Double;
 protected
 
 public
   class function GetCoordinatesFromAddress(address, api_key :String) :Tcoordinates;
   class function GetAddressMapURL(address, api_key :String) :String;
+  class function GetDistanceBetween(origem, destino, api_key :String) :Double;
   class function GetDirectionsMap(origem :String; destino :String; pontos :Array of String; api_key :String) :String;
   class function GetDirectionsShareLink(origem :String; destino :String; pontos :Array of String) :String;
 published
@@ -145,8 +147,6 @@ begin
   str.Add( ' }); } ');
   str.Add( ' </script> ');
 
-  str.SaveToFile('C:\tt.html');
-
   Result := str.text;
 end;
 
@@ -157,11 +157,49 @@ var
   url, url_base :String;
 begin
   IdURI   := TIdURI.Create();
-
+  { TODO : add waypoints }
   url_base   := 'https://www.google.com/maps/dir/';
   url := IdURI.URLEncode(url_base + origem + '/' + destino);
 
   Result := url;
+end;
+
+class function TGoogleMapsService.GetDistanceBetween(origem, destino, api_key: String): Double;
+var
+  IdSSLIOHandlerSocket1 :TIdSSLIOHandlerSocketOpenSSL;
+  IdHTTP1 :TIdHTTP;
+  IdURI :TIdURI;
+  xml, url, url_base, url_origem, url_destino, url_key :String;
+begin
+  IdSSLIOHandlerSocket1 := TIdSSLIOHandlerSocketOpenSSL.Create(nil);
+  IdHTTP1 := TIdHTTP.Create(nil);
+  IdURI   := TIdURI.Create();
+
+  try
+    IdSSLIOHandlerSocket1.SSLOptions.Method := sslvTLSv1;
+    IdSSLIOHandlerSocket1.SSLOptions.Mode   := sslmUnassigned;
+    IdHTTP1.IOHandler := IdSSLIOHandlerSocket1;
+
+
+    IdHTTP1.Request.Accept := 'text/html, */*';
+    IdHTTP1.Request.UserAgent := 'Mozilla/3.0 (compatible; IndyLibrary)';
+    IdHTTP1.Request.ContentType := 'application/x-www-form-urlencoded';
+    IdHTTP1.HandleRedirects := True;
+
+
+    url_base    := 'https://maps.googleapis.com/maps/api/distancematrix/xml?';
+    url_origem  := 'origins=' + origem;
+    url_destino := '&destinations=' + destino;
+    //url_key     := '&key=' + api_key;
+    url_key     := '&' + api_key;
+    url := IdURI.URLEncode(url_base + origem + url_destino + url_key);
+
+    xml := UTF8Decode(IdHTTP1.Get(url));
+    Result := ParseDistancesXML(xml);
+  finally
+    IdSSLIOHandlerSocket1.Free;
+    IdHTTP1.Free;
+  end;
 end;
 
 class function TGoogleMapsService.ParseCoordinatesXML(xml: String): TCoordinates;
@@ -184,6 +222,41 @@ begin
 
     Result.latitude  := StrToFloat(location.ChildNodes['lat'].Text);
     Result.longitude := StrToFloat(location.ChildNodes['lng'].Text);
+  finally
+    XMLDocument1 := nil;
+    DecimalSeparator := ',';
+  end;
+end;
+
+class function TGoogleMapsService.ParseDistancesXML(xml: String): Double;
+var
+  XMLDocument1 :IXMLDocument;
+  row, element, duration, distance, response :Ixmlnode;
+  status :String;
+begin
+  { TODO : Fix-me - Remove DecimalSeparator }
+  DecimalSeparator := '.';
+  XMLDocument1 := TXMLDocument.Create(nil);
+  try
+    //Remove first tag
+    Delete(xml, 1, pos('?>', xml)+2);
+    XMLDocument1.LoadFromXML(StripNonXml(xml));
+    response := XMLDocument1.DocumentElement.ChildNodes.FindNode('DistanceMatrixResponse') ;
+
+    //
+    status := response.ChildNodes['status'].Text;
+
+    //
+    row := response.ChildNodes.FindNode('row');
+    element := row.ChildNodes.FindNode('element');
+
+    //Duration
+    duration := element.ChildNodes.FindNode('duration');
+
+    //Distance
+    distance := element.ChildNodes.FindNode('distance');
+
+    Result := StrToFloat(distance.ChildNodes['value'].Text);
   finally
     XMLDocument1 := nil;
     DecimalSeparator := ',';
