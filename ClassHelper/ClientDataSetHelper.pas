15fa0f0ae564096a8971551e1a6dfd9f37f77df1
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-03-13 08:33:07 -0300

    Add ClientDatasetHelper

---
+++ ClassHelper/ClientDataSetHelper.pas
@@ -0,0 +1,22 @@
+unit ClientDataSetHelper;
+
+interface
+
+uses DbClient;
+
+type TClientDataSetHelper = class helper for TClientDataSet
+  procedure InitializeDataSet;
+end;
+
+implementation
+
+{ TClientDataSetHelper }
+procedure TClientDataSetHelper.InitializeDataSet;
+begin
+  if Self.Active then
+    Self.EmptyDataSet
+  else
+    Self.CreateDataSet;
+end;
+
+end.
